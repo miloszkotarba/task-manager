fe04174501df9b4d44b4fdda8f9e362315b2a17c
Miłosz Kotarba <111527970+miloszkotarba@users.noreply.github.com>
2023-11-26 17:55:36 +0100

    App update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 const PORT = 3000
 
@@ -10,6 +12,16 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tasks', tasks)
 
-app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
 
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,9 +3,27 @@ const getAllTasks = (req, res) => {
 }
 
 const createTask = (req, res) => {
-
+    res.send(req.body)
 }
+
+const getTask = (req, res) => {
+    res.json({
+        id: req.params.id
+    })
+}
+
+const updateTask = (req, res) => {
+    res.send('Update Task')
+}
+const deleteTask = (req, res) => {
+    res.send('Delete Task')
+}
+
 
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
