5c891a9004dea8f8331acdccbaeaa648969e4f74
Miłosz Kotarba <111527970+miloszkotarba@users.noreply.github.com>
2023-11-26 21:21:07 +0100

    App update

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,29 +1,69 @@
-const getAllTasks = (req, res) => {
-    res.send('All items from the file.')
+const Task = require('../models/Task')
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
-const createTask = (req, res) => {
-    res.send(req.body)
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
-const getTask = (req, res) => {
-    res.json({
-        id: req.params.id
-    })
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOne({ _id: taskID })
+
+        if (!task)
+            return res.status(404).json({ msg: `No task with ID: ${taskID}` })
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID })
+
+        if (!task)
+            return res.status(404).json({ msg: `No task with ID: ${taskID}` })
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('Update Task')
-}
-const deleteTask = (req, res) => {
-    res.send('Delete Task')
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true
+            // override: true -- PUT REQUEST (replace all items with new data; if you do not provide the previous value of a field, Mongoose will delete the previous field)
+        })
+
+        if (!task)
+            return res.status(404).json({ msg: `No task with ID: ${taskID}` })
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask,
+    getAllTasks, createTask, getTask, updateTask, deleteTask,
 }
